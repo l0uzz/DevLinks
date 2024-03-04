1de3771bef0b3d8d4cb4a2e6b41e0781e78b816e
L0uzzDev <l0uzzdev@gmail.com>
2024-03-04 09:35:24 -0300

    Add SweetAlert JS , Fix avatar.png , add notify for page , Fix padding for page in browser for pc or cellphone

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-function yeah() {
-  alert("Welcome to the page");
-    
-}
-yeah();
+swal.fire({
+  title: 'Seja bem-vindo',
+  text: 'L0uzz lhe da as saudações. Utilize dos Botões para pegar a informação que precisa.',
+  icon: 'info'});
